301887f4f3247505a3f785dcbf6f6cf7f0e9a379
DtPhat <dtphatkh@gmail.com>
2022-11-21 09:36:43 +0700

    Update

---
+++ src/components/Background.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+export default function Background(props) {
+    return (
+        <div class="area">
+            <ul class="circles">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+            {props.children}
+        </div>
+    )
+}
